f9d613a5024d0202d707933b61cb7980972b7d76
Gustavo Marques <gustavosmarquesf@gmail.com>
2022-06-12 17:32:13 -0300

    basic options

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
 import './App.css';
 
 function App() {
-	// const [scope, setScope] = useState(0);
-	const [teste, setTeste] = useState(false);
+	const [scope, setScope] = useState(0);
+	const [graphData, setGraphData] = useState({});
+	const [randomGraphValue, setRandomGraphValue] = useState(null);
+	const [visibileInput, setVisibleInput] = useState(false);
+
+	const [enableZoom, setEnableZoom] = useState(false);
 
 	function genRandomTree(N = 10, reverse = false) {
 		return {
@@ -13,36 +17,64 @@ function App() {
 				.filter(id => id)
 				.map(id => ({
 					[reverse ? 'target' : 'source']: id,
-					[reverse ? 'source' : 'target']: Math.round(Math.random() * (id - 1))
+					[reverse ? 'source' : 'target']: Math.round(Math.random() * (id - 1)),
+					color: '#888888'
 				}))
 		};
 	}
 
-	useEffect(() => {
-		if (!teste) setTeste(true);
-	}, [teste])
+	function newRamdomGraph() {
+		setGraphData(randomGraphValue ? genRandomTree(parseInt(randomGraphValue)) : genRandomTree());
+		setScope(1);
+	}
 
-	return (
+	if (scope === 1) return (
 		<div className="App">
 			<header className="App-header">
 				<ForceGraph2D
-					graphData={genRandomTree()}
-					linkWidth={5}
-					linkColor='#ffffff'
+					graphData={graphData}
+					linkWidth={3}
+					enableZoomInteraction={enableZoom}
+					height='700'
 				/>
+
+				<section>
+					<h4> Options: </h4>
+					<form>
+						<span>
+							<input type='checkbox' onChange={() => setEnableZoom(!enableZoom)} /> <label> Enable zoom </label>
+						</span>
+						<br />
+						<button onClick={() => setScope(0)} > Go back </button>
+					</form>
+				</section>
 			</header>
 		</div>
 	)
 
-
-	// else return (
-	// 	<div className="App">
-	// 		<header className="App-header">
-	// 			<button onClick={() => setScope(1)} > New force graph </button>
-	// 			<button> Upload json </button>
-	// 		</header>
-	// 	</div>
-	// );
+	else return (
+		<div className="App">
+			<header onMouseLeave={() => setVisibleInput(false)} className="App-header">
+				<button onClick={() => setScope(1)} > New empty force graph </button>
+				<button
+					onClick={newRamdomGraph}
+					onMouseOver={() => setVisibleInput(true)}
+				>
+					New random force graph
+				</button>
+				{
+					visibileInput &&
+					<input
+						value={randomGraphValue}
+						onMouseOver={() => setVisibleInput(true)}
+						placeholder='Node number'
+						type='number'
+						onChange={(e) => setRandomGraphValue(e.target.value)} />
+				}
+				<button> Upload json </button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
