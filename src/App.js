6bf39325c2604f44b922dfb65d042bff959fda85
Gustavo Marques <gustavosmarquesf@gmail.com>
2022-06-13 21:12:51 -0300

    remove node func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify'
 import ForceGraph2D from 'react-force-graph-2d';
 import Options from './ForceGraphOptions';
 import NodeForm from './NodeForm';
@@ -6,7 +7,7 @@ import './App.css';
 
 function App() {
 	const [scope, setScope] = useState(0);
-	const [graphData, setGraphData] = useState({});
+	const [graphData, setGraphData] = useState({ nodes: [], links: [] });
 	const [randomGraphValue, setRandomGraphValue] = useState(null);
 	const [visibileInput, setVisibleInput] = useState(false);
 
@@ -14,12 +15,16 @@ function App() {
 	const [moving, setMoving] = useState(false);
 	const [nodeDrag, setNodeDrag] = useState(false);
 	const [showLabels, setShowLabels] = useState(false);
+	const [ticks, setTicks] = useState(100);
 
 	const [showModal, setShowModal] = useState(false);
 
 	const [nodeName, setNodeName] = useState('');
 	const [nodeValue, setNodeValue] = useState('');
 	const [nodeColor, setNodeColor] = useState('');
+
+	const [backupData, setBackupData] = useState({ nodes: [], links: [] });
+	const [isRemoving, setIsRemoving] = useState(false);
 
 	function genRandomTree(N = 10, reverse = false) {
 		return {
@@ -58,6 +63,7 @@ function App() {
 	}
 
 	const addNode = () => {
+		setTicks(1);
 		const node = {
 			id: graphData?.nodes?.length,
 			val: nodeValue || 1,
@@ -78,6 +84,37 @@ function App() {
 		setShowModal(false);
 	}
 
+	const activeRemoving = () => {
+		setTicks(1);
+		if (!isRemoving) {
+			setIsRemoving(true);
+			const backup = { ...graphData };
+			setBackupData(backup);
+
+			return toast('Select nodes to remove', { toastId: 'removing-node', autoClose: false, closeButton: false, closeOnClick: false })
+		}
+	}
+
+	const inactiveRemoving = (cancel = false) => {
+		if (cancel) setGraphData(backupData);
+		setIsRemoving(false);
+		toast.dismiss();
+	}
+
+	const removeNode = (node) => {
+		const updateNode = graphData.nodes.filter(item => item.id !== node.id);
+		const updateLinks = graphData.links.filter(item => item.target?.id !== node.id && item.source?.id !== node.id);
+
+		const dataToUpdate = { ...graphData };
+		dataToUpdate.nodes = updateNode;
+		dataToUpdate.links = updateLinks;
+
+		setGraphData(dataToUpdate);
+	}
+
+	const handleNodeClick = (node) => {
+		if (isRemoving) return removeNode(node);
+	}
 
 	if (scope === 1) return (
 		<>
@@ -89,8 +126,10 @@ function App() {
 						enableZoomInteraction={enableZoom}
 						enablePanInteraction={moving}
 						enableNodeDrag={nodeDrag}
-						height={700}
+						height={500}
 						nodeCanvasObject={showLabels ? setNodesLabels : null}
+						onNodeClick={handleNodeClick}
+						cooldownTicks={ticks}
 						onNodeDragEnd={node => {
 							node.fx = node.x;
 							node.fz = node.z;
@@ -102,13 +141,26 @@ function App() {
 					<NodeForm
 						onSubmit={addNode}
 						states={{ showModal, setShowModal, nodeName, setNodeName, nodeValue, setNodeValue, nodeColor, setNodeColor }} />
-
-					<button onClick={() => setShowModal(true)} > Insert new node </button>
+					<span>
+						<button onClick={() => setShowModal(true)} > Insert new node </button>
+						{
+							!isRemoving ?
+								<button onClick={activeRemoving} > Remove node </button>
+								: <> <button onClick={() => inactiveRemoving(false)} > Salve changes </button>
+									<button onClick={() => inactiveRemoving(true)} > cancel </button>
+								</>
+						}
+					</span>
+					<span>
+						<button onClick={() => setShowModal(true)} > Insert links </button>
+						<button onClick={() => setShowModal(true)} > Remove links </button>
+					</span>
 					<br />
 					<button onClick={() => setScope(0)} > Go back </button>
 
 				</header>
 			</div>
+			<ToastContainer />
 		</>
 	)
 
